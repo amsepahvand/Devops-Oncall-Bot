0436d214cc9f26ab6ddc3d8ed47c8471d5972b1a
Amir <123931830+amsepahvand@users.noreply.github.com>
2024-09-08 20:05:17 +0330

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import sqlite3
 import jdatetime
 from datetime import datetime
 import pytz
+import logging
 
 def create_db():
     if not os.path.exists('bot-db.db'):
@@ -19,13 +20,14 @@ def create_db():
         c.execute('''
             CREATE TABLE IF NOT EXISTS oncall_staff (
                 user_id INTEGER PRIMARY KEY,
+                name TEXT,
                 username TEXT
             )
         ''')
         c.execute('''
             CREATE TABLE IF NOT EXISTS user_state (
                 user_id INTEGER PRIMARY KEY,
-                state TEXT NOT NULL,
+                state TEXT,
                 message TEXT
             )
         ''')
@@ -37,6 +39,18 @@ def create_db():
         c.execute('''
             CREATE TABLE IF NOT EXISTS bot_api_token (
                 token TEXT
+            )
+        ''')
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS schedule_setting (
+                setting_value INTEGER DEFAULT 1
+            )
+        ''')
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS oncall_history (
+                name TEXT,
+                username TEXT,
+                date TEXT
             )
         ''')
         conn.commit()
@@ -55,27 +69,23 @@ def get_api_token():
     c.execute('SELECT token FROM bot_api_token')
     result = c.fetchone()
     conn.close()
-    return result[0] if result else None  # Return None if no token found
+    return result[0] if result else None 
 
 def get_bot_owner_id():
     conn = sqlite3.connect('bot-db.db')
     c = conn.cursor()
-    c.execute('SELECT user_id FROM bot_owner')
+    c.execute('SELECT user_id FROM bot_owner WHERE user_id IS NOT NULL AND user_id != ""')
     result = c.fetchone()
     conn.close()
-    return result[0] if result else None  # Return None if no owner ID found
+    return result[0] if result else None
 
-
-# Function to store messages in the SQLite database
 def store_message(user_id, username, message):
     conn = sqlite3.connect('bot-db.db')
     c = conn.cursor()
     
-    # Get the current time in Asia/Tehran timezone
     tehran_tz = pytz.timezone('Asia/Tehran')
     tehran_time = datetime.now(tehran_tz)
     
-    # Get the current Persian date
     persian_date = jdatetime.datetime.fromgregorian(
         year=tehran_time.year,
         month=tehran_time.month,
@@ -89,30 +99,27 @@ def store_message(user_id, username, message):
     conn.commit()
     conn.close()
 
-# Function to add on-call staff
-def add_oncall_staff(user_id, username):
+def add_oncall_staff(user_id, name, username):
     conn = sqlite3.connect('bot-db.db')
     c = conn.cursor()
-    c.execute('INSERT OR IGNORE INTO oncall_staff (user_id, username) VALUES (?, ?)', (user_id, username))
+    c.execute('INSERT OR IGNORE INTO oncall_staff (user_id, name, username) VALUES (?, ?, ?)', (user_id, name, username))
     conn.commit()
     conn.close()
 
-# Function to remove on-call staff
+def get_oncall_list():
+    conn = sqlite3.connect('bot-db.db')
+    c = conn.cursor()
+    c.execute('SELECT user_id, name, username FROM oncall_staff')
+    staff = c.fetchall()
+    conn.close()
+    return staff
+
 def remove_oncall_staff(user_id):
     conn = sqlite3.connect('bot-db.db')
     c = conn.cursor()
     c.execute('DELETE FROM oncall_staff WHERE user_id = ?', (user_id,))
     conn.commit()
     conn.close()
-
-# Function to get the list of on-call staff
-def get_oncall_staff():
-    conn = sqlite3.connect('bot-db.db')
-    c = conn.cursor()
-    c.execute('SELECT user_id, username FROM oncall_staff')
-    staff = c.fetchall()
-    conn.close()
-    return staff
 
 # Function to update user state
 def update_user_state(user_id, state, message=None):
@@ -123,16 +130,47 @@ def update_user_state(user_id, state, message=None):
     conn.commit()
     conn.close()
 
-# Function to get user state
+
 def get_user_state(user_id):
     conn = sqlite3.connect('bot-db.db')
     c = conn.cursor()
     c.execute('SELECT state, message FROM user_state WHERE user_id = ?', (user_id,))
     result = c.fetchone()
     conn.close()
-    return result if result else (None, None)  # Return (None, None) if no state found
+    return result[0] if result else (None, None)  
+
+def set_schedule_setting(setting_value):
+    conn = sqlite3.connect('bot-db.db')
+    c = conn.cursor()
+    c.execute('UPDATE schedule_setting SET setting_value = ? WHERE setting_value IS NOT NULL ', (setting_value,))
+    conn.commit()
+    conn.close()
+
+
+def get_schedule_setting():
+    conn = sqlite3.connect('bot-db.db')
+    c = conn.cursor()
+    c.execute('SELECT setting_value FROM schedule_setting')
+    result = c.fetchone()
+    conn.close()
+    return result[0] if result else None  
+
+def add_oncall_history(name, username, date):
+    conn = sqlite3.connect('bot-db.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO oncall_history (name, username, date) VALUES (?, ?, ?)', (name, username, date))
+    conn.commit()
+    conn.close()
+
+def get_oncall_history():
+    conn = sqlite3.connect('bot-db.db')
+    c = conn.cursor()
+    c.execute('SELECT name, username, date FROM oncall_history')
+    history = c.fetchall()
+    conn.close()
+    return history
 
 create_db()
 
 
-
+#        logging.info(f"{oncall_userid} - 22222222222222222222222222222222222") 
